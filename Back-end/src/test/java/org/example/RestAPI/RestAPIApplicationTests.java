b925c404b777fb75ef24f167a8e7fdde18793140
legianha592 <doantrihung592@gmail.com>
2021-04-19 14:28:14 +0700

    add tests

---
+++ Back-end/src/test/java/org/example/RestAPI/RestAPIApplicationTests.java
@@ -0,0 +1,11 @@
+package org.example.RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestAPIApplicationTests {
+    public static void main(String[] args){
+        SpringApplication.run(RestAPIApplicationTests.class, args);
+    }
+}
