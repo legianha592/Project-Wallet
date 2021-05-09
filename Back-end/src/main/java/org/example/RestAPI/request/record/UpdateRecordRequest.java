4f664675c433568d299021d5935acf00a0656212
legianha <doantrihung592@gmail.com>
2021-05-09 10:30:29 +0700

    fix record requests

--- Back-end/src/main/java/org/example/RestAPI/request/record/UpdateRecordRequest.java
+++ Back-end/src/main/java/org/example/RestAPI/request/record/UpdateRecordRequest.java
@@ -1,9 +1,11 @@
 package org.example.RestAPI.request.record;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.example.RestAPI.finalstring.FinalMessage;
 
 @Data
+@RequiredArgsConstructor
 public class UpdateRecordRequest {
     private final int MAX_LENGTH = 50;
     private long record_id;
@@ -12,16 +14,6 @@ public class UpdateRecordRequest {
     private double amount;
     private long typeRecord_id;
     private String result;
-
-    public UpdateRecordRequest(long record_id, String title, String note, double amount, long typeRecord_id) {
-        this.record_id = record_id;
-        this.title = title;
-        this.note = note;
-        this.amount = amount;
-        this.typeRecord_id = typeRecord_id;
-        this.checkValidRequest();
-    }
-
     private void checkValidRequest(){
         if (title.length() > MAX_LENGTH){
             result = FinalMessage.INVALID_TITLE_RECORD_LENGTH;
