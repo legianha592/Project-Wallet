a426ab2161c7aa6bda22ce42d5a88d1b7a6e92f2
Dao Minh Quan <daominhquan176@gmail.com>
2021-07-08 15:39:56 +0700

    add .env file

--- Front-end/reactjs-app/src/utils/constants.js
+++ Front-end/reactjs-app/src/utils/constants.js
@@ -1,6 +1,6 @@
-export const WALLET_ROOT_URL = "http://localhost:8080/wallet";
-export const RECORD_ROOT_URL = "http://localhost:8080/record";
-export const TYPE_RECORD_ROOT_URL = "http://localhost:8080/typeRecord";
-export const USER_ROOT_URL = "http://localhost:8080/user";
+export const WALLET_ROOT_URL = `${process.env.REACT_APP_API_SERVER_DOMAIN}/wallet`;
+export const RECORD_ROOT_URL = `${process.env.REACT_APP_API_SERVER_DOMAIN}/record`;
+export const TYPE_RECORD_ROOT_URL = `${process.env.REACT_APP_API_SERVER_DOMAIN}/typeRecord`;
+export const USER_ROOT_URL = `${process.env.REACT_APP_API_SERVER_DOMAIN}/user`;
 export const TAB_RECORDS = 1;
 export const TAB_WALLETS = 2;
